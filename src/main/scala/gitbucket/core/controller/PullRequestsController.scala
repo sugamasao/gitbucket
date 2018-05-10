8e0374524b3db63f4e3637a6c9362f7a608fe640
sugamasao <sugamasao@gmail.com>
2018-05-10 17:29:52 +0900

    pr-menu show count

--- src/main/scala/gitbucket/core/controller/PullRequestsController.scala
+++ src/main/scala/gitbucket/core/controller/PullRequestsController.scala
@@ -113,12 +113,13 @@ trait PullRequestsControllerBase extends ControllerBase {
         val name = repository.name
         getPullRequest(owner, name, issueId) map {
           case (issue, pullreq) =>
-            val (commits, _) =
+            val (commits, diffs) =
               getRequestCompareInfo(owner, name, pullreq.commitIdFrom, owner, name, pullreq.commitIdTo)
 
             html.conversation(
               issue,
               pullreq,
+              diffs,
               commits.flatten,
               getPullRequestComments(owner, name, issue.issueId, commits.flatten),
               getIssueLabels(owner, name, issueId),
@@ -157,23 +158,25 @@ trait PullRequestsControllerBase extends ControllerBase {
   })
 
   get("/:owner/:repository/pull/:id/commits")(referrersOnly { repository =>
-    params("id").toIntOpt.flatMap { issueId =>
-      val owner = repository.owner
-      val name = repository.name
-      getPullRequest(owner, name, issueId) map {
-        case (issue, pullreq) =>
-          val (commits, _) =
-            getRequestCompareInfo(owner, name, pullreq.commitIdFrom, owner, name, pullreq.commitIdTo)
+    params("id").toIntOpt.flatMap {
+      issueId =>
+        val owner = repository.owner
+        val name = repository.name
+        getPullRequest(owner, name, issueId) map {
+          case (issue, pullreq) =>
+            val (commits, diffs) =
+              getRequestCompareInfo(owner, name, pullreq.commitIdFrom, owner, name, pullreq.commitIdTo)
 
-          html.commits(
-            issue,
-            pullreq,
-            commits,
-            getPullRequestComments(owner, name, issue.issueId, commits.flatten),
-            isManageable(repository),
-            repository
-          )
-      }
+            html.commits(
+              issue,
+              pullreq,
+              diffs,
+              commits,
+              getPullRequestComments(owner, name, issue.issueId, commits.flatten),
+              isManageable(repository),
+              repository
+            )
+        }
     } getOrElse NotFound()
   })
 
